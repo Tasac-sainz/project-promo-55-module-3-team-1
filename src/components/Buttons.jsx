1e435fcce1bb1a982c8ca3a42a41c62807d6301b
Silvia Benitez Masip <silviam92@gmail.com>
2025-10-21 20:21:34 +0200

    create buttons

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,25 @@
+import "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+const Buttons = ({ to, children }) => {
+  return (
+    <>
+      <Link to={to}>
+        <button>{children}</button>
+      </Link>
+    </>
+  );
+};
+
+Buttons.propTypes = {
+  to: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default Buttons;
+
+// Cómo lo voy a usar
+// <Buttons to="project-list">Ver Proyectos</Buttons>
+// <Buttons to="/">Nuevo proyecto</Buttons>
+// <Buttons to="your-project">Crea tu proyecto</Buttons>
