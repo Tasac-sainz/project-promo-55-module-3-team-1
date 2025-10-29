9035cf2f68afb02018f3fd25fce44db5d8cbaeba
Alba Díaz Melián <albadiazgraficaimpresa@gmail.com>
2025-10-29 18:34:21 +0000

    pages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/project-promo-55-module-3-team-1/",
   plugins: [react()],
+  base: "/project-promo-55-module-3-team-1/",
   server: {
     open: "/",
     watch: {
